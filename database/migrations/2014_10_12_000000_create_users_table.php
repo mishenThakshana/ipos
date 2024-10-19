0eecda5c0f8c7b8dc69604b494e6409a0bbad5f3
mishenThakshana <mishenthak@outlook.com>
2024-10-19 14:56:03 +0530

    Update user table migration to set default value for 'type' column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('type'); //super,admin,manager,cashier
+            $table->string('type')->default('admin'); //super,admin,manager,cashier
             $table->rememberToken();
             $table->timestamps();
         });
